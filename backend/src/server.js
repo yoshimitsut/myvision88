e1b1bbd4de71d4646eb4bd8e3f437723d44f9a17
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-28 17:02:26 -0300

    alterando upload de arquivo

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 app.use('/image', express.static('image'))
 
