80f02b3cb0d84bcc70dbd0698be796c996609b22
yoshimitsut <shimitsutanaka@gmail.com>
2026-03-05 15:30:46 -0300

    alteração na url da img

---
+++ frontend/src/pages/StoreManagement.tsx
@@ -0,0 +1,25 @@
+// interface StoreInfo {
+//   id: number;
+//   name?: string;
+//   logo_url?: string | null;
+//   favicon_url?: string | null;
+//   phone?: string | null;
+//   store_mail?: string | null;
+//   adress?:string | null;
+//   adress_postal_code?: string | null;
+//   busines_hours_days?: string | null;
+//   busines_hours_close?: string | null;
+//   busines_hours_breack?: string | null;
+// }
+
+const StoreManagement: React.FC = () => {
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default StoreManagement;
