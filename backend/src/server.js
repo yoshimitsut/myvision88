4c4f79e6353246533ad4c711ab40f1dea403038c
Yoshimitsut <shimitsutanaka@gmail.com>
2026-09-14 20:44:09 -0300

    arrumando bug

--- backend/src/server.js
+++ backend/src/server.js
@@ -59,7 +59,15 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
+// Interceptador robusto para requisições de preflight (OPTIONS)
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
